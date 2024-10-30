7cb4d750b8e74e1ed889e6c41be8b45c7e950edd
Artem <artemkolisnychenko@gmail.com>
2024-10-30 16:14:38 +0200

    Add files via upload

---
+++ Task1/Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Server
+{
+    TcpListener Listener;
+
+    public Server(int Port)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Listener = new TcpListener(IPAddress.Any, Port);
+        Listener.Start();
+
+        Console.WriteLine("Сервер увімкненй...");
+
+        while (true)
+        {
+            TcpClient client = Listener.AcceptTcpClient();
+            Console.WriteLine("Кліент під'єднанний!");
+
+            Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+            clientThread.Start(client);
+        }
+    }
+
+    private void HandleClient(object clientObject)
+    {
+        TcpClient client = (TcpClient)clientObject;
+        NetworkStream stream = client.GetStream();
+
+        string message = "Hello";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Відправленно до клієнта " + message);
+
+        stream.Close();
+        client.Close();
+        Console.WriteLine("З'єднання з клієнтом від'єднано");
+    }
+
+    ~Server()
+    {
+        if (Listener != null)
+        {
+            Listener.Stop();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        new Server(80);
+    }
+}
